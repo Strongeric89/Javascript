e3223aeee124cd4ae5278f00d73c7a3c65e2a735
Eric Strong <ericstrong89@gmail.com>
2017-11-24 16:47:22 +0000

    JS basics

---
+++ popups/external.js
@@ -0,0 +1,14 @@
+//alerts
+alert('works');
+
+//confirms
+var x = confirm('do you know javascript?');
+if(x == true){
+  document.write('well done!');
+}else{
+  document.write('sorry ;(');
+}
+
+//prompts
+var y = prompt('enter your favourite language');
+document.write(y);
